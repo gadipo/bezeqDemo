a9f35cfb71be925abef5a6f45230fe9c71432b1b
gadipo <58295522+gadipo@users.noreply.github.com>
2025-12-28 02:07:48 +0200

    make rest billing client a spring bean

--- src/main/java/com/ashgan/bezeqDemo/Client/REST/BillingClient.java
+++ src/main/java/com/ashgan/bezeqDemo/Client/REST/BillingClient.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Component
 public class BillingClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BillingClient.class);
